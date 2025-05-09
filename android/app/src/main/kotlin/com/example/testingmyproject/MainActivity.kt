859c764ecd41d4ad5479bcb1fca730a4b5d64438
unknown <Talha khan>
2025-05-09 22:39:17 +0500

    Final Year Project Testing

---
+++ android/app/src/main/kotlin/com/example/testingmyproject/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.testingmyproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MYTestingFYP_Project());
+}
+
+class MYTestingFYP_Project extends StatefulWidget {
+  const MYTestingFYP_Project({super.key});
+
+  @override
+  State<MYTestingFYP_Project> createState() => _MYTestingFYP_ProjectState();
+}
+
+class _MYTestingFYP_ProjectState extends State<MYTestingFYP_Project> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
